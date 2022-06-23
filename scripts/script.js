5c6edbbeb7cdff705d69f24d4a4daa4e428acc5f
Shada <43104909+shada12@users.noreply.github.com>
2022-06-23 00:34:29 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -16,76 +16,68 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
-            return "You are equal!";
+            return "You are equal! your score is: "+ playerScore +" combuter score is: "+ computerScore;
         } else if (computerSelection === "paper") {
             computerScore++;
-            return "Computer Win! Paper beats Rock.";
+            return "Computer Win! Paper beats Rock.  your score is: "+ playerScore +" combuter score is: "+ computerScore;
         } else {
             playerScore++;
-            return "You Win! Rock beats Scissors";
+            return "You Win! Rock beats Scissors.  your score is: "+ playerScore +" combuter score is: "+ computerScore;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             playerScore++;
-            return "You Win! Paper beats Rock";
+            return "You Win! Paper beats Rock.  your score is: "+ playerScore +" combuter score is: "+ computerScore;
         } else if (computerSelection === "paper") {
-            return "You are equal!";
+            return "You are equal!  your score is: " + playerScore +" combuter score is: "+ computerScore;
 
         } else {
             computerScore++;
-            return "Computer Win! Scissors beats Paper";
+            return "Computer Win! Scissors beats Paper.  your score is: " + playerScore +" combuter score is: "+ computerScore;
         }
     } else if (playerSelection === "scissors") {
 
         if (computerSelection === "rock") {
             computerScore++;
-            return "Computer Win! Rock beats Scissors";
+            return "Computer Win! Rock beats Scissors. your score is: "+ playerScore +" combuter score is: "+ computerScore;
         } else if (computerSelection === "paper") {
             playerScore++;
-            return "You Win! Scissors beats Paper";
+           result= "You Win! Scissors beats Paper. your score is: "+ playerScore +" combuter score is: "+ computerScore;
+            return result;
         } else {
-            return "You are equal!";
+            result="You are equal!  your score is: "+ playerScore +" combuter score is: "+ computerScore;
+            return result;
         }
     }
-
 }
-
 
 let round = 1;
 function game() {
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt("Please select: rock, paper or scissors?").toLowerCase();
         const computerSelection = computerPlay().toLowerCase();
-        console.log("Round " + round + ": " + playRound(playerSelection, computerSelection) + "<br>");
-        /*document.write("Round "+ round +": "+ playRound(playerSelection, computerSelection)+"<br>");*/
-
+        console.log("Round " + round + ": " + playRound(playerSelection, computerSelection));
+        
         round++;
-
     }
 
-    console.log("<br>");
-    console.log("Your Score is: " + playerScore + " <br>");
-    console.log("Computer Score: " + computerScore + " <br><br>");
-
-    /* document.write("<br>");
-     document.write("Your Score is: " + playerScore + "<br>");
-     document.write("Computer Score: " + computerScore + "<br><br>");*/
+    console.log("Your final result is: " + playerScore);
+    console.log("Computer final result is : " + computerScore);
 
     if (playerScore > computerScore) {
-        console.log("You are the winner! <br>");
-        /*  document.write("You are the winner! <br>");*/
+        console.log("You are the winner!");
+        
 
     } else if (playerScore < computerScore) {
 
-        console.log("Computer is the winner! <br>");
-        /*   document.write("Computer is the winner! <br>");*/
+        console.log("Computer is the winner!");
+       
 
     } else if (playerScore == computerScore) {
 
-        console.log("You are equal! <br>");
-        /* document.write("You are equal! <br>");*/
+        console.log("You are equal!");
+       
     }
-
 }
 
 game();
